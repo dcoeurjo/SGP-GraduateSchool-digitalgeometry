0c04d3d7b98fb42fb9f6489ee4fe099672a42624
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2021-06-14 15:12:12 +0200

    dt example

--- dt.cpp
+++ dt.cpp
@@ -5,10 +5,14 @@
 #include <DGtal/helpers/StdDefs.h>
 #include <DGtal/helpers/Shortcuts.h>
 #include <DGtal/helpers/ShortcutsGeometry.h>
+
+#include <DGtal/images/SimpleThresholdForegroundPredicate.h>
+#include <DGtal/geometry/volumes/distance/DistanceTransformation.h>
+#include <DGtal/geometry/volumes/distance/VoronoiMap.h>
+
 #include "polyscope/polyscope.h"
 #include "polyscope/point_cloud.h"
 #include "polyscope/surface_mesh.h"
-
 
 #include "deps/CLI11/CLI11.hpp"
 
@@ -19,10 +23,66 @@ using namespace Z3i;
 // Using standard 3D digital space.
 typedef Shortcuts<Z3i::KSpace>         SH3;
 typedef ShortcutsGeometry<Z3i::KSpace> SHG3;
+int slice;
+int axis;
+bool negate=false;
+
+CountedPtr<SH3::BinaryImage> binary_image;
+
+typedef functors::SimpleThresholdForegroundPredicate<SH3::BinaryImage> Predicate;
+typedef DistanceTransformation< Z3i::Space, Predicate, Z3i::L2Metric> DT;
+
+
+template<typename Image>
+void registerDTSlice(const Image& image, const Domain &domain, Z3i::Integer slice, size_t axis, bool inner)
+{
+  std::vector<Point> grid;
+  std::vector<double> values;
+  for(auto y = domain.lowerBound()[1]; y <= domain.upperBound()[1] ; ++y )
+    for(auto z = domain.lowerBound()[2]; z <= domain.upperBound()[2] ; ++z )
+    {
+      double val;
+      switch (axis) {
+        case 0:
+          val =image(Point(slice, y, z));grid.push_back(Point(slice, y, z));
+          break;
+        case 1: val=image(Point(y,slice, z));grid.push_back(Point(y,slice, z));break;
+        case 2: val = image(Point(y, z, slice));grid.push_back(Point(y,z,slice));break;
+        default:
+          break;
+      }
+      values.push_back(val);
+    }
+  auto pspcl= polyscope::registerPointCloud("Slice", grid);
+  auto q=pspcl->addScalarQuantity("DT values", values);
+  if (inner)
+    pspcl->setPointRadiusQuantity(q,false);
+}
+
+
 
 void myCallback()
 {
-  
+  ImGui::SliderInt("Slice", &slice, binary_image->domain().lowerBound()[0], binary_image->domain().upperBound()[0]);
+  ImGui::SliderInt("Axis", &axis, 0,2);
+  ImGui::Checkbox("Outside DT", &negate);
+  if (ImGui::Button("Update slice dt"))
+  {
+    //DT
+    Z3i::L2Metric l2;
+    Predicate binaryshape(*binary_image, 0);
+    if (negate)
+    {
+      functors::NotPointPredicate<Predicate> negpred(binaryshape);
+      DistanceTransformation< Z3i::Space, functors::NotPointPredicate<Predicate>, Z3i::L2Metric>  distancemap(binary_image->domain(), negpred, l2);
+      registerDTSlice(distancemap, binary_image->domain(), slice,axis, false);
+    }
+    else
+    {
+      DT distancemap(binary_image->domain(), binaryshape, l2);
+      registerDTSlice(distancemap, binary_image->domain(), slice,axis,true);
+    }
+  }
 }
 
 int main(int argc, char **argv)
@@ -39,13 +99,12 @@ int main(int argc, char **argv)
   auto h=1.; //gridstep
   params("surfaceComponents", "All");
   
-  auto binary_image = SH3::makeBinaryImage(filename, params );
-  auto K               = SH3::getKSpace( binary_image );
-  auto surface         = SH3::makeDigitalSurface( binary_image, K, params );
-  auto surfels         = SH3::getSurfelRange( surface, params );
-  auto embedder   = SH3::getCellEmbedder( K );
+  binary_image = SH3::makeBinaryImage(filename, params );
+  auto K            = SH3::getKSpace( binary_image );
+  auto surface      = SH3::makeDigitalSurface( binary_image, K, params );
+  auto surfels      = SH3::getSurfelRange( surface, params );
+  auto embedder     = SH3::getCellEmbedder( K );
   
-
   //Need to convert the faces
   std::vector<std::vector<size_t>> faces;
   std::vector<RealPoint> positions;
@@ -60,12 +119,9 @@ int main(int argc, char **argv)
     cpt+=4;
     faces.push_back(face);
   }
-  
   auto primalSurf = polyscope::registerSurfaceMesh("Primal surface",positions, faces)->setEdgeWidth(1.0)->setEdgeColor({1.,1.,1.});
   
   polyscope::state::userCallback = myCallback;
   polyscope::show();
-  
-  
   return 0;
 }
