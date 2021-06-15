6f3ecb830fb24ae8df7b9f4b28fb4ef4411b9a4b
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2021-06-15 14:20:03 +0200

    Readme

--- dt.cpp
+++ dt.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <array>
+#include <utility>
 
 #include <DGtal/base/Common.h>
 #include <DGtal/helpers/StdDefs.h>
@@ -26,7 +28,8 @@ typedef Shortcuts<Z3i::KSpace>         SH3;
 typedef ShortcutsGeometry<Z3i::KSpace> SHG3;
 int slice;
 int axis;
-bool negate=false;
+bool negate=true;
+bool subSample=false;
 
 CountedPtr<SH3::BinaryImage> binary_image;
 
@@ -42,36 +45,74 @@ VoroMapOutside *voronoiMapOutside;
 
 
 template<typename Image>
-void registerDTSlice(const Image& image, const Domain &domain,
-                     Z3i::Integer slice, size_t axis, bool inner)
+void registerSlice(const Image& image, const Domain &domain,
+                     Z3i::Integer slice, size_t axis, bool inner,
+                   bool subSample)
 {
+  Point p;
+  auto dim1 = (axis+1)%3;
+  auto dim2 = (axis+2)%3;
+  auto width1 = domain.upperBound()[dim1] - domain.lowerBound()[dim1] + 1;
+  auto width2 = domain.upperBound()[dim2] - domain.lowerBound()[dim2] + 1;
+  unsigned int cpt=0u;
+  
+  if (subSample)
+  {
+    width1 /= 2;
+    width2 /= 2;
+  }
+  
+  std::vector<Point> vertices(width1*width2);
+  std::vector<double> values(width1*width2);
+  std::vector<unsigned int> sites(width1*width2);
+  std::vector<std::array<unsigned int,4>> faces(width1*width2*2);
+
   std::vector<Point> grid;
-  std::vector<double> values;
-  std::vector<unsigned int> sites;
-    Point p;
-  for(auto y = domain.lowerBound()[1]; y <= domain.upperBound()[1] ; ++y )
-    for(auto z = domain.lowerBound()[2]; z <= domain.upperBound()[2] ; ++z )
+  std::vector<double> radii;
+  
+  for(auto v = domain.lowerBound()[dim2]; v <= domain.upperBound()[dim2] ; ++v )
+  {
+    for(auto u = domain.lowerBound()[dim1]; u <= domain.upperBound()[dim1] ; ++u )
     {
       switch (axis) {
-        case 0: p=Point(slice, y, z);break;
-        case 1: p=Point(y,slice, z);break;
-        case 2: p=Point(y, z, slice);break;
-        default:
-          break;
+        case 0: p=Point(slice, u, v);break;
+        case 1: p=Point(u,slice, v);break;
+        case 2: p=Point(u, v, slice);break;
       }
-      Point q = image(p);
-      if (p!=q)
+      Point q = image(p); //Voronoi site
+      vertices[cpt] = p;
+      if ((u <  domain.upperBound()[dim1] ) && (v < domain.upperBound()[dim2]))
+      {
+        if ((subSample) && ( u+1<domain.upperBound()[dim1] ) && (v+1 < domain.upperBound()[dim2]))
+          faces[cpt] = {cpt, cpt + 1, cpt + width1+1 , cpt+width1};
+        else
+          faces[cpt] = {cpt, cpt + 1, cpt + width1+1 , cpt+width1};
+
+      }
+      values[cpt]=((p-q).norm()); //Distance
+      sites[cpt]=(13*(q[0] + q[1]*451 + q[2]*311) % 1024); //Voronoi cells
+      if ((inner)&& (p!=q))
       {
         grid.push_back(p);  //Grid
-        sites.push_back(13*(q[0] + q[1]*451 + q[2]*311) % 1024); //Voronoi cells
-        values.push_back((p-q).norm()); //Distance
+        radii.push_back((p-q).norm()); //Distance
       }
+      ++cpt;
+      if (subSample)
+        ++u;
     }
-  auto pspcl= polyscope::registerPointCloud("Slice", grid);
-  auto q=pspcl->addScalarQuantity("DT values", values);
-  pspcl->addScalarQuantity("Sites", sites);
-  if (inner) //For inner balls, we display them with non scaled radius
+    if (subSample)
+      ++v;
+  }
+  
+  auto psm = polyscope::registerSurfaceMesh("Slice", vertices, faces);
+  psm->addVertexDistanceQuantity("DT", values);
+  psm->addVertexScalarQuantity("Sites", sites);
+  if (inner)
+  {
+    auto pspcl= polyscope::registerPointCloud("Balls", grid);
+    auto q=pspcl->addScalarQuantity("DT values", radii);
     pspcl->setPointRadiusQuantity(q,false);
+  }
 }
 
 
@@ -86,17 +127,16 @@ void myCallback()
   ImGui::RadioButton("Y axis", &axis, 1); ImGui::SameLine();
   ImGui::RadioButton("Z axis", &axis, 2);
   
-  
-  
   ImGui::Checkbox("Outside DT", &negate);
+  ImGui::Checkbox("SubSample (for OpenGL prefs)", &subSample);
   if (ImGui::Button("Update slice dt"))
   {
     //DT
     Z3i::L2Metric l2;
     if (negate)
-      registerDTSlice(*voronoiMapOutside, binary_image->domain(), slice,axis, false);
+      registerSlice(*voronoiMapOutside, binary_image->domain(), slice,axis, false,subSample);
     else
-      registerDTSlice(*voronoiMap, binary_image->domain(), slice,axis,true);
+      registerSlice(*voronoiMap, binary_image->domain(), slice,axis,true,subSample);
   }
   
   if (ImGui::Button("RDMA"))
@@ -106,9 +146,7 @@ void myCallback()
     {
       auto q = voronoiMap->operator()(p);
       if (p!=q)
-      {
         squareDT.setValue(p, (p-q).squaredNorm());
-      }
     }
     typedef PowerMap<ImageContainerBySTLVector<Domain, uint64_t>, L2PowerMetric> PowerMapType;
     PowerMapType powermap(binary_image->domain(), squareDT, Z3i::l2PowerMetric);
@@ -116,7 +154,7 @@ void myCallback()
     std::vector<Point> centers;
     std::vector<double> radii;
     for(auto &p: rdma.domain())
-    {
+    {		
       if (rdma(p) != 0)
       {
         centers.push_back(p);
@@ -136,7 +174,7 @@ void myCallback()
     slice++;
     if (slice > binary_image->domain().upperBound()[axis])
       slice = binary_image->domain().lowerBound()[axis];
-    registerDTSlice(*voronoiMapOutside, binary_image->domain(), slice,axis, false);
+    registerSlice(*voronoiMapOutside, binary_image->domain(), slice,axis, false,subSample);
   }
 }
 
